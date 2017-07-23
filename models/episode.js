8c92f10ae9d30ac9099992f856a602ab32eb1cbe
OverFlow636 <OverFlow636@gmail.com>
2017-07-23 12:29:22 -0400

    cleanup

--- models/episode.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-  var Episode = app.Waterline.Collection.extend({
-    identity: 'episode',
-    connection: 'myLocalDisk',
-    schema: false
-  });
-  app.orm.loadCollection(Episode);
-};
